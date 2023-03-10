67e8c2bddc9d74c70960eba6ddad00b8a3374edc
whalefall123456 <2945955711@qq.com>
2023-03-10 18:26:27 +0800

    新增数据库和其他角色语音，防止api挂掉

--- __init__.py
+++ __init__.py
@@ -2,7 +2,7 @@ import random
 from nonebot import on_command
 from nonebot.adapters.onebot.v11 import Message, GroupMessageEvent, MessageSegment
 from pathlib import Path
-from src.plugins.nonebot_plugin_asoul.utils import get_json
+from src.plugins.nonebot_plugin_asoul.utils import get_json, get_local_voice, get_api_voice, get_url_db
 
 path: Path = Path(__file__).parent
 res_path = path / "resource"
@@ -10,7 +10,7 @@ voice_path = res_path / "audio"
 
 voice = on_command("语音包")
 
-
+#语音包指令处理模块
 @voice.handle()
 async def _(event:GroupMessageEvent):
     message = event.get_plaintext().split()[1:]
@@ -20,19 +20,16 @@ async def _(event:GroupMessageEvent):
         data = get_json()
         #如果角色名字在json中
         if character in data:
-            data_chac:dict = data.get(character)
-            #给了角色参数，语音参数，并且在json文件中
-            if len(message) > 1 and message[1] != "随机" and message[1] in data_chac:
-                voice_value = voice_path / character / data_chac[message[1]]
-                await voice.finish(MessageSegment.record(voice_value))
-            #如果没给参数，或者参数是随机
-            else:
-                data_list = list(data_chac.values())
-                random_num = random.randint(0,len(data_list))
-                voice_value = voice_path / character / data_list[random_num]
-                await voice.finish(MessageSegment.record(voice_value))
+            #调用内置语音包发送方法
+            await voice.finish(get_local_voice(data,character,message))
+        elif character in data["API"]:
+            #调用api获取语音并发送
+            api = data["API"][character]
+            await voice.finish(await get_api_voice(api))
+        elif character in data["DB"]:
+            #从数据库查取地址
+            await voice.finish(await get_url_db(character))
         else:
             await voice.finish(MessageSegment.text("角色语音包不存在"))
     else:
         await voice.finish(MessageSegment.text("请给出角色名"))
-
--- utils.py
+++ utils.py
@@ -1,5 +1,11 @@
+import io
 import json
+import random
+import sqlite3
 from pathlib import Path
+from httpx import AsyncClient
+from nonebot.adapters.onebot.v11 import MessageSegment
+
 path: Path = Path(__file__).parent
 res_path = path / "resource"
 voice_path = res_path / "audio"
@@ -9,3 +15,81 @@ def get_json():
     with open(res_path / "data.json",encoding='utf-8') as user_file:
         data:dict = json.load(user_file)
         return data
+
+
+#内置语音包的发送方法
+def get_local_voice(data:dict,character:str,message:list):
+    data_chac:dict = data.get(character)
+    #给了角色参数，语音参数，并且在json文件中
+    if len(message) > 1 and message[1] != "随机" and message[1] in data_chac:
+        voice_value = voice_path / character / data_chac[message[1]]
+        return MessageSegment.record(voice_value)
+    #如果没给参数，或者参数是随机
+    else:
+        data_list = list(data_chac.values())
+        random_num = random.randint(0,len(data_list))
+        voice_value = voice_path / character / data_list[random_num]
+        return MessageSegment.record(voice_value)
+
+#下载音频
+async def download_voice(url: str, client: AsyncClient):
+    try:
+        headers = {
+            "Referer": url,
+            "User-Agent": "Mozilla/5.0 (Windows NT 10.0; WOW64) "
+            "AppleWebKit/537.36 (KHTML, like Gecko) Chrome/56.0.2924.87 Safari/537.36",
+        }
+        re = await client.get(url=url, headers=headers, timeout=60)
+        if re.status_code == 200:
+            return re.content
+        else:
+            return re.status_code
+    except:
+        return 408
+
+#调用api获取语音包发送方法
+async def get_api_voice(api:str):
+    async with AsyncClient() as client:
+        content = await download_voice(api,client)
+        if type(content) == int:
+            return MessageSegment.text("语音包下载出错")
+        else:
+            return MessageSegment.record(io.BytesIO(content))
+
+#从数据库中读取url并下载语音
+async def get_url_db(name:str):
+    conn = sqlite3.connect(res_path / "voicedb.db")
+    cur = conn.cursor()
+    cursor = cur.execute("select * from urldata where name = ?",[name])
+    result = cursor.fetchall()
+    #获取随机数
+    random_num = random.randint(0, len(result))
+    url = result[random_num][5]
+    return await get_api_voice(url)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
